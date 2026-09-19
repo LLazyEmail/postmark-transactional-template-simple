89da1d0fbe039b1a70743c48e93cfe3296346e82
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-19 04:17:04 +0300

    Create index.ts

---
+++ packages/template-runtime-display/src/index.ts
@@ -0,0 +1,20 @@
+// packages/template-runtime-display/src/index.ts
+
+export {
+  displayHead,
+  displayMain,
+  displayFooter,
+  displayBody,
+} from './renderers';
+
+export type {
+  HtmlString,
+  DisplayHeadProps,
+  DisplayMainProps,
+  DisplayFooterProps,
+  DisplayBodyProps,
+  DisplayHeadRenderer,
+  DisplayMainRenderer,
+  DisplayFooterRenderer,
+  DisplayBodyRenderer,
+} from './types';
