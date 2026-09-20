b6a89588724a5b891cca4356b50fa632208bb319
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-20 03:58:45 +0300

    chore: add tsconfig.json and tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.js',
+    templates: 'src/index2.ts',
+  },
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'dist',
+  target: 'node18',
+  splitting: false,
+  treeshake: true,
+});
